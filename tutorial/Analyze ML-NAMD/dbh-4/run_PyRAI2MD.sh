14d1331c9d43a3062e043e418a2b429dca894106
Leticia Adao Gomes <100699955+adaogomesl@users.noreply.github.com>
2026-03-11 03:31:18 -0400

    Add files via upload

---
+++ tutorial/Analyze ML-NAMD/dbh-4/run_PyRAI2MD.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+## script for PyRAI2MD
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2-00:00:00
+#SBATCH --job-name=dbh-4
+#SBATCH --partition=lopez,short,long
+#SBATCH --mem=110mb
+#SBATCH --output=%j.o.slurm
+#SBATCH --error=%j.e.slurm
+
+export INPUT=input
+export WORKDIR=/projects/lopez/share_from_Leticia/ml-nac/NEQUIP/on-paper-models-train-before-hpo/dbh-4traj-and-90b-60a/4traj-and-90b-60a-pc/2026-01-23-16-55-49-946501/500-namd/dbh-4
+
+cd $WORKDIR
+pyrai2md $INPUT
+
